ab97033f8957006f09352c2696a323188163eeb6
Katerina Polakova <katerina.polakova@outlook.com>
2023-04-12 09:41:43 +0200

    Start vers

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
+import cv2
+
+def recognition():
+    result()
+
+def result(): # print result with parameters
     print("All LEDS on")
+    pass
+
+def read_img():
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        recognition()
+        if key == 27: # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
 
 
 if __name__ == "__main__":
-    main()
+    read_img()
