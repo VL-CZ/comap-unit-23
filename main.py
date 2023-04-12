c49bfd191b70165974e7152c2a0d8d1718acae56
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-04-12 09:35:46 +0200

    basic skeleton

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+def take_screenshot():
+    return None
+
+def center_image(image):
+    return None
+
+def detect_led(image):
+    return None
+
+def detect_display(image):
+    return None
+
 def main():
+    screenshot = take_screenshot()
+    centered = center_image(screenshot)
+    result = detect_led(centered)
+    result = detect_display(centered)
     print("All LEDS on")
 
 
