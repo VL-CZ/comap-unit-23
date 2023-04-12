542cb33e01ae8910d16043b010eb2a57ce349d9c
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-04-12 15:21:38 +0200

    LED colors done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 from centering import centering
 import matplotlib.pyplot as plt
 import numpy as np
-from recognition import recognition
+from recognition import recognize_led_colors, Led, Color
 
 
 def result():  # print result with parameters
@@ -24,7 +24,7 @@ def read_img():
         cv2.imshow("preview", frame)
         rval, frame = vc.read()
         key = cv2.waitKey(20)
-        recognition()
+        recognize_led_colors()
         result()
         if key == 27:  # exit on ESC
             break
@@ -34,4 +34,17 @@ def read_img():
 
 
 if __name__ == "__main__":
-    read_img()
+    # read_img()
+    leds = [Led(68, 297), Led(96, 300), Led(176, 300), Led(371, 300), Led(454, 297), Led(486, 297)]
+    colors = [Color.GREEN, Color.RED]
+
+    for i in range(2, 18):
+        img_path = os.path.join(os.getcwd(), f'images/framedisp_manual_{i}.jpg')
+        image = cv2.imread(img_path)
+
+        statuses = recognize_led_colors(image, leds, colors)
+        statuses_str = [f'LED {i}: {s.name}' for i, s in enumerate(statuses)]
+        print(statuses_str)
+
+        cv2.imshow('IMG', image)
+        cv2.waitKey(0)
--- recognition.py
+++ recognition.py
@@ -1,31 +1,92 @@
+from enum import Enum
+
 import cv2
+import numpy as np
 
 
-def recognition(image):
-    cv2.imshow('IMG', image)
+class Led:
+    def __init__(self, x, y):
+        self.x, self.y, self.diameter = x, y, 25
 
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+
+
+class LedStatus(Enum):
+    RED = 1
+    GREEN = 2
+    OFF = 3
+    ON = 4
+
+
+def _is_pixel_empty(pixel):
+    return pixel[0] == pixel[1] == pixel[2] == 0
+
+
+def _get_mask(image, color):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # GREEN
-    green_led_lower, green_led_upper = (36, 25, 25), (70, 255, 255)
-    green_led_mask = cv2.inRange(hsv_image, green_led_lower, green_led_upper)
+    if color == Color.RED:
+        mask1 = cv2.inRange(hsv_image, (0, 50, 20), (5, 255, 255))
+        mask2 = cv2.inRange(hsv_image, (175, 50, 20), (180, 255, 255))
 
-    res = cv2.bitwise_and(image, image, mask=green_led_mask)
-    cv2.imshow("Green mask", res)
+        return cv2.bitwise_or(mask1, mask2)
+    elif color == Color.GREEN:
+        green_led_lower, green_led_upper = (36, 25, 25), (70, 255, 255)
+        return cv2.inRange(hsv_image, green_led_lower, green_led_upper)
+    else:
+        return None
 
-    # RED
-    mask1 = cv2.inRange(hsv_image, (0, 50, 20), (5, 255, 255))
-    mask2 = cv2.inRange(hsv_image, (175, 50, 20), (180, 255, 255))
 
-    red_led_mask = cv2.bitwise_or(mask1, mask2)
+def _has_color(mask, led, color):
+    empty, colored = 0, 0
+    for x in range(led.x - led.diameter // 2, led.x + led.diameter // 2):
+        for y in range(led.y - led.diameter // 2, led.y + led.diameter // 2):
+            c = mask[y, x]
+            if _is_pixel_empty(c):
+                empty += 1
+            else:
+                colored += 1
 
-    res = cv2.bitwise_and(image, image, mask=red_led_mask)
-    cv2.imshow("Red mask", res)
+            # cv2.imshow("Display mask", mask)
+            # cv2.waitKey(0)
+    colored_ratio = colored / (colored + empty)
+    return colored_ratio >= 0.2
+
+
+def recognize_led_colors(image, leds, possible_colors):
+    # cv2.imshow('IMG', image)
+
+    masks = {}
+
+    for color in possible_colors:
+        mask = _get_mask(image, color)
+        masks[color] = cv2.bitwise_and(image, image, mask=mask)
+        # cv2.imshow("Mask", res)
+
+    led_statuses = [LedStatus.OFF] * len(leds)
+    i = 0
+
+    for led in leds:
+        for color in possible_colors:
+            if _has_color(masks[color], led, color):
+                led_statuses[i] = LedStatus(color.value)
+        i += 1
 
     # DISPLAY MASK
-    display_mask = cv2.inRange(hsv_image, (0, 0, 168), (172, 111, 255))
+    # display_mask = cv2.inRange(hsv_image, (0, 0, 168), (172, 111, 255))
+    #
+    # res = cv2.bitwise_and(image, image, mask=display_mask)
+    # cv2.imshow("Display mask", res)
+    #
+    # cv2.waitKey(0)
 
-    res = cv2.bitwise_and(image, image, mask=display_mask)
-    cv2.imshow("Display mask", res)
+    return led_statuses
 
-    cv2.waitKey(0)
+
+def recognize_leds(image, leds):
+    statuses = recognize_led_colors(image, leds, [c.value for c in Color])
+    on_off_statuses = [s if s == LedStatus.OFF else LedStatus.ON for s in statuses]
+    return on_off_statuses
