cab82debac30a562498e5124738ed9b1ee24f9b8
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-04-12 15:27:23 +0200

    real-time LEDs

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import cv2
 from centering import centering
 import matplotlib.pyplot as plt
 import numpy as np
-from recognition import recognize_led_colors, Led, Color
+from recognition import recognize_led_colors, Led, Color, recognize_leds
+
+# CONFIG
+LEDS = [Led(68, 297), Led(96, 300), Led(176, 300), Led(371, 300), Led(454, 297), Led(486, 297)]
+COLORS = [Color.GREEN, Color.RED]
 
 
-def result():  # print result with parameters
-    print("All LEDS on")
-    pass
+def result(statuses):  # print result with parameters
+    statuses_str = [f'LED {i}: {s.name}' for i, s in enumerate(statuses)]
+    print(statuses_str)
 
 
 def read_img():
@@ -24,8 +28,17 @@ def read_img():
         cv2.imshow("preview", frame)
         rval, frame = vc.read()
         key = cv2.waitKey(20)
-        recognize_led_colors()
-        result()
+
+        # LED colors
+        print('LED statuses')
+        led_colors = recognize_led_colors(frame, LEDS, COLORS)
+        result(led_colors)
+
+        # # LED statuses
+        # print('LED statuses: ')
+        # leds = recognize_leds(frame, LEDS)
+        # result(leds)
+
         if key == 27:  # exit on ESC
             break
 
@@ -35,14 +48,12 @@ def read_img():
 
 if __name__ == "__main__":
     # read_img()
-    leds = [Led(68, 297), Led(96, 300), Led(176, 300), Led(371, 300), Led(454, 297), Led(486, 297)]
-    colors = [Color.GREEN, Color.RED]
 
     for i in range(2, 18):
         img_path = os.path.join(os.getcwd(), f'images/framedisp_manual_{i}.jpg')
         image = cv2.imread(img_path)
 
-        statuses = recognize_led_colors(image, leds, colors)
+        statuses = recognize_led_colors(image, LEDS, colors)
         statuses_str = [f'LED {i}: {s.name}' for i, s in enumerate(statuses)]
         print(statuses_str)
 
